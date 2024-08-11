d4bd55f7d216b267f7ca079e6c570ee5d7e131f9
Ganesh Singh Bisht <ganeshbest20@gmail.com>
2024-08-11 13:32:14 +0530

    Adding Course selling project details

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -65,7 +65,7 @@ export function ProjectCard({
             src={image}
             alt={title}
             width={500}
-            height={300}
+            height={0}
             className="h-40 w-full overflow-hidden object-cover object-top"
           />
         )}
